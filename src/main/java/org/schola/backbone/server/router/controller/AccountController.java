1767a5f4a08937d7a1666c842de9ff83f94130ce
Simão Dias <105567918+simaodiazz@users.noreply.github.com>
2024-10-17 17:12:05 +0100

    Started Controller's and Handler's

--- src/main/java/org/schola/backbone/server/router/controller/AccountController.java
+++ src/main/java/org/schola/backbone/server/router/controller/AccountController.java
@@ -2,6 +2,7 @@ package org.schola.backbone.server.router.controller;
 
 import org.schola.backbone.server.data.model.Account;
 import org.schola.backbone.server.data.service.AccountDataService;
+import org.schola.backbone.server.data.service.resolver.AccountInputResolvers;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
@@ -12,15 +13,16 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/account")
 public class AccountController {
 
-    private AccountDataService service;
+    private final AccountDataService service;
 
     @Autowired
     public AccountController(AccountDataService service) {
         this.service = service;
     }
 
-    @GetMapping
+    @GetMapping // TODO
     public ResponseEntity<Account> getAccount() {
-
+        final Account account = service.get(AccountInputResolvers.ID, 1L);
+        return ResponseEntity.ok(account);
     }
 }
